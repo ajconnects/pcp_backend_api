28a4e19517b1be4cbfb7bde2a202ec6e83648fc9
Aj connect <ajiriiviero2021@gmail.com>
2024-07-26 22:20:58 +0200

    new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,15 +47,16 @@ INSTALLED_APPS = [
     'messaging',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 AUTH_USER_MODEL = 'api.User'
@@ -73,7 +74,6 @@ CORS_ALLOW_HEADERS = [
     'content-type',
     'authorization',
     'Access-Control-Allow-Origin',
-    ...
 ]
 
 CORS_ALLOW_METHODS = [
